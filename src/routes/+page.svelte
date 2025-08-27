103e2bfdd2e4113259cfbf413af6da192c9e11c5
Leftium <381217+Leftium@users.noreply.github.com>
2025-08-27 17:45:52 -0500

    Display basic logos.

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,37 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<script lang="ts">
+	import logoGlow from '$lib/assets/logo-layers/glow.webp';
+	import logoLigature from '$lib/assets/logo-layers/ligature.webp';
+	import logoShadow from '$lib/assets/logo-layers/shadow.webp';
+	import logoSquare from '$lib/assets/logo-layers/square.webp';
+</script>
+
+<grid-logo>
+	<img src={logoGlow} alt="" />
+	<img src={logoShadow} alt="" />
+	<img src={logoSquare} alt="" />
+	<img src={logoLigature} alt="" />
+</grid-logo>
+
+<grid-logo class="dark">
+	<img src={logoGlow} alt="" />
+	<img src={logoShadow} alt="" />
+	<img src={logoSquare} alt="" />
+	<img src={logoLigature} alt="" />
+</grid-logo>
+
+<style>
+	grid-logo {
+		display: grid;
+		place-items: center;
+
+		img {
+			grid-column: 1 / 2;
+			grid-row: 1 / 2;
+			height: 50vh;
+		}
+
+		&.dark {
+			background-color: black;
+		}
+	}
+</style>
